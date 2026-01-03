b78bd8f017c89f6953d39e146181be9778b1ecda
lopezolidev <olivera9806@gmail.com>
2026-01-02 23:27:07 -0400

    modified set predicate and flatten without using ! or \+ operands

--- learnPrologNow/6/flatten.pl
+++ learnPrologNow/6/flatten.pl
@@ -1,5 +1,39 @@
-flatten([], []).
-flatten(X, [X | R]) :- flatten(_ , R).
-flatten([H | T], [H | F]) :-
-    flatten(H, F) ,
-    flatten(T, F) .
+%flatten with append
+
+%base case
+flatten([],[]).
+
+% Head is a list (Double recursion w/ append)
+flatten([H | T], R) :-
+    is_list(H), % native predicate that checks if element is a list
+    ! , % if true, we commit to this rule
+    flatten(H , FlatH), % flatting the 2º list
+    flatten(T, FlatT), % we flat the rest
+    append(FlatH, FlatT, R). % append the results
+
+% Head is a simple element (atom)
+flatten([H | T], [H | FlatT]) :-
+    flatten(T, FlatT). % only process the rest.
+
+
+% flatten without append
+
+% wrapper
+flattenV2(List, Result) :-
+    aplanar(List, [], Result).
+
+
+% Rule 1: base case -> if first list goes empty, result is the same as accumulator
+aplanar([], Acc, Acc). 
+
+% Rule 2: processed element is a list. First the tail will be processed and passed onto ResCola
+% then we will process head of the list and paste it with ResCola to become result
+aplanar([H | T], Acc, Result) :-
+    aplanar(T, Acc, ResCola), % processing the end first
+    aplanar(H, ResCola, Result). % pasting that end result with the head
+
+% Rule 3: processed element is an atom. An atom is not an empty list nor a list
+% with a single element
+aplanar(X, Acc, [X | Acc]) :-
+    X \= [] ,
+    X \= [_ | _]. % clause that validates X is not a list
--- learnPrologNow/6/set.pl
+++ learnPrologNow/6/set.pl
@@ -3,3 +3,23 @@ set([H | T] , [H | B]) :-   \+ member(H, T),
                             set(T, B).
 set([H | T] , B) :- member(H, T) ,
                     set(T, B) , !.
+
+% set v2 -> without using \+ or !
+
+%base case
+setV2([], []).
+
+% itsNot: auxiliar predicate to determine an element is not present in a list
+itsNot(_, []) :- !. % if different to every element of the list
+itsNot(X, [Y | T]) :- 
+    X \= Y ,
+    itsNot(X, T). % recursive case
+
+% when H is not repeated in T
+setV2([H | T], [H | Rest]) :-
+    itsNot(H, T),
+    setV2(T, Rest).
+
+% when H is present in T
+setV2([_ | T], Rest) :-
+    set(T, Rest).
