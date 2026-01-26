0d8dab5c5db2f531a8ef02e358cd2dd82efea1fc
lopezolidev <olivera9806@gmail.com>
2026-01-26 08:20:07 -0400

    learned how to alter trees data structures when substituting repetitions of one tree over other

---
+++ university/24.pl
@@ -0,0 +1,19 @@
+/*
+24. Defina predicado sustituir(X,Y,ArbolX,ArbolY) donde ArbolY es el
+resultado de sustituir las apariciones del término X por Y en ArbolX.
+*/
+
+% si es nil, ya es una verdad.
+sustituir(X, Y, nil, nil) .
+
+% si son iguales los nodos raíz de los subárboles, los modificamos
+sustituir(X, Y, arbol(X, Izq, Der), arbol(Y, Izq2, Der2)) :-
+    sustituir(X, Y, Izq, Izq2) ,
+    sustituir(X, Y, Der, Der2) , !.
+    
+
+% si no son iguales los nodos raíz, no hacemos nada
+sustituir(X, Y, arbol(X, Izq, Der), arbol(Z, Izq2, Der2)) :-
+    sustituir(X, Y, Izq, Izq2) ,
+    sustituir(X, Y, Der, Der2) .
+
